d4a329c691215fd538de82fd9dd27be5d36687c8
ratsila484 <tsilavina484@gmail.com>
2026-04-28 22:51:36 +0200

    ex0

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -1,70 +1,107 @@
-#!/usr/bin/python3
+#!/usr/env/bin/python3
 
-from abc import ABC, abstractmethod
+import abc
 from typing import Any
 
 
-class DataProcessor():
+def check_is_int(data: Any) -> bool:
+    try:
+        int(data)
+        return True
+    except ValueError:
+        return False
+
+
+def check_is_float(data: Any) -> bool:
+    try:
+        float(data)
+        return True
+    except ValueError:
+        return False
+
+
+def check_is_list(data: Any) -> bool:
+    try:
+        len(data)
+        return True
+    except Exception:
+        return False
+
+
+class NotValidateData(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class DataProcessor(abc.ABC):
     def __init__(self) -> None:
         self.data: str = ""
+        self.use_validate: bool = False
     
-    @abstractmethod
+    @abc.abstractmethod
     def validate(self, data: Any) -> bool:
-        pass
-    
-    @abstractmethod
+        self.use_validate = True
+        is_valid: bool = False
+        if check_is_list(data):
+            for number in data:
+                try:
+                    number = str(number)
+                    int(number)
+                    float(number)
+                    is_valid = True
+                except ValueError:
+                    is_valid = False
+        else:
+            data = str(data)
+            if check_is_int(data) or check_is_float(data):
+                is_valid = True
+            else:
+                is_valid = False
+        return is_valid
+
+
+    @abc.abstractmethod
     def ingest(self, data: Any) -> None:
         pass
-
-    def output(self) -> tuple[int, str]:
-        pass
+    
+    def output(self, data: Any) -> tuple[int, str]:
+        if (check_is_list(data)):
+            print(f"Extracting {len(data)} values")
+            for i in range(data):
+                print(f"Numeric value {i}: {data[i]}")
+        else:
+            print(f"Extracting {data}")
+            print(f"Numeric value: {data}")
 
 
 class NumericProcessor(DataProcessor):
     def __init__(self) -> None:
-        self.data: int | float | list[int | float] = 0
-        self.is_validate: bool = False
-
-    def validate(self, data: Any) -> bool:
-        isNumeric: bool = False
-        if data:
-            try:
-                int(data)
-                float(data)
-                isNumeric = True
-            except ValueError:
-                isNumeric = False
-        elif len(data) >= 1:
-            for number in data:
-                if number.isdigit() or float(number):
-                    isNumeric += True
-                else:
-                    isNumeric += False
-        return isNumeric
-
-    def ingest(self, data: Any) -> None:
-        if not self.is_validate:
-            if 
-            raise 
-
-
-class TextProcessor(DataProcessor):
-    def validate(self, data: Any) -> bool:
-        pass
+        super().__init__()
     
-    def ingest(self, data: Any) -> None:
-        pass
+    def validate(self, data):
+        return super().validate(data)
 
-
-class LogProcessor(DataProcessor):
-    def validate(self, data: Any) -> bool:
-        pass
-    
-    def ingest(self, data: Any) -> None:
-        pass
+    def ingest(self, data):
+        if not self.use_validate:
+            raise NotValidateData("Improper numeric data")
+        else:
+            self.data = data
 
 
 if __name__ == "__main__":
-    num = NumericProcessor()
-    print(num.validate("42.6fdsdf"))
-    num.ingest(42)
+    print("===  Code Nexus - Data Processor ===\n")
+    try:
+        print("Testing Numeric Processor...")
+        num1 = NumericProcessor()
+        num2 = NumericProcessor()
+        print(f"Trying to validate input '42': {num1.validate(42)}")
+        print(f"Trying to validate input 'Hello': {num1.validate('Hello')}")
+        print(f"Test invalid ingestion of string 'foo' without prior validation:")
+        num2.ingest('foo')
+        num3 = NumericProcessor()
+        data = [1,2,3,4,5]
+        num3.validate(data)
+        num3.ingest(data)
+        num3.output(data)
+    except Exception as ex:
+        print(f"Got exception: {ex}")
